a964f254445cf2ffb8f405df8964e6ec33da2ebf
Hirozy <yoohwzy@gmail.com>
2018-08-08 20:14:58 +0800

    Enable fuzzy matching for helm-mode

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -15,6 +15,13 @@
 (setq helm-autoresize-min-height 10)
 (helm-autoresize-mode 1)
 
+;; To globally enable fuzzy matching for helm-mode
+(setq helm-mode-fuzzy-match t)
+(setq helm-completion-in-region-fuzzy-match t)
+
+;; Set matching limit
+(setq helm-candidate-number-limit 50)
+
 (helm-mode 1)
 
 (provide 'init-helm)
