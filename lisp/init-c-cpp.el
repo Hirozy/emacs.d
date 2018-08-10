5e609b2e931bf68ce96aeea52507803a5c8c316c
Hirozy <me@hirozy.com>
2018-08-10 23:26:23 +0800

    Added C/CPP completion mode choice (cquery)

--- lisp/init-c-cpp.el
+++ lisp/init-c-cpp.el
@@ -1,6 +1,6 @@
-(require-packages '(lsp-mode
-                    cquery
-                    smartparens))
+(require-packages '(smartparens))
+
+(setq c-cpp-completion-mode-value "cquery")
 
 (setq c-basic-offset 4)
 
@@ -10,16 +10,24 @@
     (add-hook 'c-mode-hook #'smartparens-mode)
     (add-hook 'c++-mode-hook #'smartparens-mode))
 
-(setq cquery-path (concat dotfiles-dir "site-lisp/package/cquery/build/cquery"))
+(defun c-cpp-completion-mode/cquery ()
+    (require-packages '(lsp-mode
+                        lsp-ui
+                        cquery))
 
-(use-package cquery
-    :commands lsp-cquery-enable
-    :init
-    (setq cquery-executable cquery-path)
-    (setq cquery-cache-dir "/tmp/")
-    (add-hook 'c-mode-hook 'lsp-cquery-enable)
-    (add-hook 'c++-mode-hook 'lsp-cquery-enable)
-    ;; https://github.com/cquery-project/emacs-cquery/issues/45#issuecomment-406813724
-    (add-hook 'lsp-after-open-hook 'lsp-ui-mode))
+    (setq cquery-path (concat dotfiles-dir "site-lisp/package/cquery/build/cquery"))
+
+    (use-package cquery
+        :commands lsp-cquery-enable
+        :init
+        (setq cquery-executable cquery-path)
+        (setq cquery-cache-dir "/tmp/")
+        (add-hook 'c-mode-hook 'lsp-cquery-enable)
+        (add-hook 'c++-mode-hook 'lsp-cquery-enable)
+        ;; https://github.com/cquery-project/emacs-cquery/issues/45#issuecomment-406813724
+        (add-hook 'lsp-after-open-hook 'lsp-ui-mode)))
+
+(cond ((string= c-cpp-completion-mode-value "cquery")
+          (funcall 'c-cpp-completion-mode/cquery)))
 
 (provide 'init-c-cpp)
