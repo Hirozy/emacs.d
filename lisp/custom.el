f18e91684b0113cceeae179bf0c7c6f591ceb81e
Hirozy <me@hirozy.com>
2018-08-10 17:28:32 +0800

    Added blank line in the end of files

--- lisp/custom.el
+++ lisp/custom.el
@@ -8,12 +8,10 @@
     ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
  '(package-selected-packages
    (quote
-    (cquery company-lsp lsp-ui highlight-indent-guides which-key yasnippet-snippets yasnippet golden-ratio neotree helm-descbinds smartparens ein company-anaconda company org-plus-contrib helm-swoop helm use-package color-theme-solarized))))
+    (cquery highlight-indent-guides golden-ratio neotree which-key company-lsp lsp-ui lsp-mode ein smartparens yasnippet-snippets yasnippet company helm-descbinds helm-swoop helm org-plus-contrib color-theme-solarized use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-(provide 'custom)
