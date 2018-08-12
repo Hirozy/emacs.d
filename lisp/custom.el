dd30d2fa4541a74664eebb5a760a8e8c365580b0
Hirozy <yoohwzy@gmail.com>
2018-08-12 15:40:02 +0800

    24bit color support, replace the theme with solarized-theme

--- lisp/custom.el
+++ lisp/custom.el
@@ -5,10 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages
    (quote
-    (cquery highlight-indent-guides golden-ratio neotree which-key company-lsp lsp-ui lsp-mode ein smartparens yasnippet-snippets yasnippet company helm-descbinds helm-swoop helm org-plus-contrib color-theme-solarized use-package))))
+    (solarized-theme cquery highlight-indent-guides golden-ratio neotree which-key company-lsp lsp-ui lsp-mode ein smartparens yasnippet-snippets yasnippet company helm-descbinds helm-swoop helm org-plus-contrib use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,11 +1,11 @@
-(require-packages '(color-theme-solarized))
+(require-packages '(solarized-theme))
 
-(use-package color-theme-solarized
-    :init
-    ;; Set background to dark for color-theme-solarized
-    (set-frame-parameter nil 'background-mode 'dark)
-    (set-terminal-parameter nil 'background-mode 'dark))
+;; (use-package color-theme-solarized
+;;     :init
+;;     ;; Set background to dark for color-theme-solarized
+;;     (set-frame-parameter nil 'background-mode 'dark)
+;;     (set-terminal-parameter nil 'background-mode 'dark))
 
-(load-theme 'solarized t)
+(load-theme 'solarized-dark t)
 
 (provide 'init-theme)
