3dafdbb07246e54c8cf27a347003fc95583db7bc
Hirozy <me@hirozy.com>
2018-08-12 23:36:24 +0800

    Changed <TAB> behavior

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -18,24 +18,13 @@
 
 (setq tab-always-indent 'complete)
 
-;; https://github.com/company-mode/company-mode/wiki/Switching-from-AC#setting-up-similar-popup-behavior
-(defun my/company-visible-and-explicit-action-p ()
-(and (company-tooltip-visible-p)
-     (company-explicit-action-p)))
-
-(defun company-ac-setup ()
-    "Sets up `company-mode' to behave similarly to `auto-complete-mode'."
-    (setq company-require-match nil)
-    (setq company-auto-complete #'my/company-visible-and-explicit-action-p)
-    (setq company-frontends '(company-echo-metadata-frontend
-                              company-pseudo-tooltip-unless-just-one-frontend-with-delay
-                              company-preview-frontend))
-    (define-key company-active-map (kbd "<tab>")
-        'company-select-next-if-tooltip-visible-or-complete-selection)
-    (define-key company-active-map (kbd "TAB")
-        'company-select-next-if-tooltip-visible-or-complete-selection))
-
-(company-ac-setup)
+;; https://github.com/company-mode/company-mode/wiki/Switching-from-AC
+(eval-after-load 'company
+    '(progn
+        (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
+        (define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)
+        (define-key company-active-map (kbd "S-TAB") 'company-select-previous)
+        (define-key company-active-map (kbd "<backtab>") 'company-select-previous)))
 
 ;; disable company-mode for shell in Emacs
 (add-hook 'shell-mode-hook (lambda () 
