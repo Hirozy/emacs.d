b0ee54fa5eb3d46d60a08e04bc4555f429dbc2b9
Hirozy <yoohwzy@gmail.com>
2018-08-14 21:08:41 +0800

    Added package: linum-relative

--- lisp/init-configs.el
+++ lisp/init-configs.el
@@ -1,6 +1,7 @@
 ;; Show Line Numbers
-(when (version<= "26.0.50" emacs-version )
-  (global-display-line-numbers-mode))
+;; Use linum instead
+;; (when (version<= "26.0.50" emacs-version )
+;;   (global-display-line-numbers-mode))
 
 ;; http://ergoemacs.org/emacs/emacs_set_backup_into_a_directory.html
 ;; stop creating backup~ files
