e45aab2eb93be527c2cc36b6db5e1505e4808890
Hirozy <me@hirozy.com>
2018-08-15 15:49:39 +0800

    Fixed hydra support

--- lisp/custom.el
+++ lisp/custom.el
@@ -8,7 +8,7 @@
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages
    (quote
-    (crux linum-relative magit undo-tree avy solarized-theme cquery highlight-indent-guides golden-ratio neotree which-key company-lsp lsp-ui lsp-mode ein smartparens yasnippet-snippets yasnippet company helm-descbinds helm-swoop helm org-plus-contrib use-package))))
+    (hydra crux linum-relative magit undo-tree avy solarized-theme cquery highlight-indent-guides golden-ratio neotree which-key company-lsp lsp-ui lsp-mode ein smartparens yasnippet-snippets yasnippet company helm-descbinds helm-swoop helm org-plus-contrib use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
