0554d0e9f52d69d45b6a3bea0d95dc2c40f5b007
Hirozy <me@hirozy.com>
2018-08-15 17:23:10 +0800

    Added package: projectile

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -20,4 +20,10 @@
 
 (use-package crux)
 
+(use-package projectile
+  :config
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode 1))
+
 (provide 'init-utils)
