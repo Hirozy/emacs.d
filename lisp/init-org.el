5410c9d950fc21f88164f3628cd6e5d69c876f36
Hirozy <me@hirozy.com>
2018-08-30 00:42:15 +0800

    Modified "org-latex-classes" for org to tex

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,3 +1,32 @@
 (require-packages '(org-plus-contrib))
 
+(setq org-support-shift-select t)
+
+(use-package ox-latex
+  :config
+  (add-to-list 'org-latex-classes
+               '("article" 
+                 "\\documentclass[11pt]{article}
+\\usepackage{xltxtra,xunicode}
+\\usepackage{tikz}
+\\usetikzlibrary{arrows,shapes,chains}
+\\usepackage{amsmath}
+\\usepackage{listings}
+\\usepackage[slantfont,boldfont]{xeCJK}
+\\setCJKmainfont{Kai}
+\\setCJKmonofont{Hei}
+\\setsansfont{Trebuchet MS}
+\\usepackage{indentfirst}
+\\setlength{\\parindent}{2em}"
+                 ("\\section{%s}" . "\\section*{%s}")
+                 ("\\subsection{%s}" . "\\subsection*{%s}")
+                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+  (setq org-export-in-background t)
+  (setq org-latex-pdf-process
+        '("xelatex -interaction nonstopmode -output-directory %o %f"
+          "xelatex -interaction nonstopmode -output-directory %o %f"
+          "xelatex -interaction nonstopmode -output-directory %o %f")))
+
 (provide 'init-org)
