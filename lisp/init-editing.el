7e428cfdd8c3c230735252fc70b8d4025e159317
Hirozy <me@hirozy.com>
2018-09-23 12:21:25 +0800

    Added config file: init-editing

---
+++ lisp/init-editing.el
@@ -0,0 +1,19 @@
+(require-packages '(format-all
+                    iedit
+                    crux
+                    undo-tree))
+
+(use-package undo-tree
+  :config
+  (global-undo-tree-mode 1))
+
+;; A Collection of Ridiculously Useful eXtensions for Emacs.
+;; crux bundles a few useful interactive commands to enhance your overall Emacs experience.
+;; Such as crux-smart-kill-line, crux-smart-open-line.
+(use-package crux)
+
+(use-package format-all)
+
+(use-package iedit)
+
+(provide 'init-editing)
