6e8464b43600f34223569f8e3e21443f6444ff3d
Hirozy <me@hirozy.com>
2018-10-14 18:37:44 +0800

    Remove initial "^" input from ivy prompts

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -5,11 +5,15 @@
                     counsel-projectile))
 
 (use-package ivy
+  :bind
+  ("M-x" . counsel-M-x)
   :init
   (setq ivy-height 25
         ivy-use-virtual-buffers 1)
   (setq ivy-re-builders-alist
         '((t . ivy--regex-ignore-order)))
+  ;; disable ivy prefixes
+  (setq ivy-initial-inputs-alist nil)
   :config
   (ivy-mode 1))
 
