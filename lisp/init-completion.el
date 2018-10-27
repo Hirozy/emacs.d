0a28df8fa704ca5a12bb47f0a8a355985e2dcf39
Hirozy <yoohwzy@gmail.com>
2018-10-27 20:13:38 +0800

    Enabled flycheck for all mode

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -1,9 +1,11 @@
 (require-packages '(company
+                    flycheck
                     yasnippet
                     yasnippet-snippets))
 
-(setq-default indent-tabs-mode nil)
-(setq tab-width 4)
+(setq-default indent-tabs-mode nil
+              tab-width 4
+              tab-always-indent 'complete)
 
 (use-package company
   :init
@@ -15,23 +17,28 @@
           company-dabbrev-downcase nil)
     (add-hook 'after-init-hook 'global-company-mode)))
 
-(setq tab-always-indent 'complete)
-
 ;; https://github.com/company-mode/company-mode/wiki/Switching-from-AC
 (eval-after-load 'company
   '(progn
      (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
      (define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)
      (define-key company-active-map (kbd "S-TAB") 'company-select-previous)
-     (define-key company-active-map (kbd "<backtab>") 'company-select-previous)))
+     (define-key company-active-map (kbd "<backtab>") 'company-select-previous)
+     (define-key company-active-map (kbd "M-/") 'company-other-backend)
+     (define-key company-active-map (kbd "C-n") 'company-select-next)
+     (define-key company-active-map (kbd "C-p") 'company-select-previous)))
 
 ;; disable company-mode for shell in Emacs
-(add-hook 'shell-mode-hook (lambda () 
+(add-hook 'shell-mode-hook (lambda ()
                              (company-mode -1))
           'append)
 
 (use-package yasnippet
   :config
   (yas-global-mode 1))
+
+(use-package flycheck
+  :hook
+  (after-init . global-flycheck-mode))
 
 (provide 'init-completion)
