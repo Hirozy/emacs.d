b6dd49b59dd30dafe0a2768dcff300f2dabee4ce
Hirozy <yoohwzy@gmail.com>
2018-11-27 15:59:08 +0800

    Added language support: go, lua

---
+++ lisp/init-golang.el
@@ -0,0 +1,23 @@
+;;; init-golang.el -- golang support
+
+;;; Commentary:
+;;
+;; golang support
+;;
+
+;;; Require
+(require 'init-packages)
+
+;;; Code:
+
+(require-packages '(company-go))
+
+(use-package company-go
+  :hook
+  (go-mode . (lambda ()
+               (set (make-local-variable 'company-backends) '(company-go))
+               (company-mode))))
+
+(provide 'init-golang)
+
+;;; init-golang.el ends here
