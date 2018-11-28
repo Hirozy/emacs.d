cd310a2cd45b4f2445e9caaebc6d3cbcbed44f8e
Hirozy <me@hirozy.com>
2018-11-28 11:15:02 +0800

    Changed the way of C/C++/Python/Go compiles and runs; Removed function that defines the path; Deleted the function that generated cmake file.

--- lisp/init-tex.el
+++ lisp/init-tex.el
@@ -16,8 +16,8 @@
                     biblio-core
                     biblio))
 
-(add-hook 'LaTeX-mode-hook 
-          (lambda()
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
             (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
             (setq TeX-command-default "XeLaTeX")
             (setq TeX-save-query nil)
