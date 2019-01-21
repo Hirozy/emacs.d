ff15c614040051ffe18a6a5f7ee3756b57139010
Hirozy <me@hirozy.com>
2019-01-22 01:03:50 +0800

    Added init-projectile.el

---
+++ lisp/init-projectile.el
@@ -0,0 +1,32 @@
+;;; init-projectile.el --- projectile configuration
+
+;;; Commentary:
+;;
+;; projectile configuration
+;;
+
+;;; Require
+(require 'init-packages)
+
+;;; Code:
+
+(require-packages '(projectile
+                    counsel-projectile))
+
+(use-package projectile
+  :bind-keymap
+  (("s-p" . projectile-command-map)
+   ("C-c p" . projectile-command-map))
+  :init
+  (setq projectile-enable-caching t)
+  (setq projectile-indexing-method 'hybrid)
+  :config
+  (projectile-mode 1)
+
+  (use-package counsel-projectile
+    :config
+    (counsel-projectile-mode 1)))
+
+(provide 'init-projectile)
+
+;;; init-projectile.el ends here
