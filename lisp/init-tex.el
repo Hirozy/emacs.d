37ee6d7f0b394be2500590adbf9fe4297436df3d
Hirozy <yoohwzy@gmail.com>
2019-01-22 10:43:47 +0800

    Improve the latex configuration

--- lisp/init-tex.el
+++ lisp/init-tex.el
@@ -16,41 +16,35 @@
                     biblio-core
                     biblio
                     latex-extra
-                    cdlatex
-                    company-math))
+                    cdlatex))
 
-(use-package tex
+(use-package LaTeX-mode
+  :mode ("\\.tex\\'" . LaTeX-mode)
   :hook (LaTeX-mode . (lambda ()
-                        (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))))
-
-  :init
-  (setq TeX-command-default "XeLaTeX")
-  (setq TeX-command-Show "XeLaTeX")
+                        (add-to-list 'TeX-command-list
+                                     '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
+                        (setq-local TeX-command-default "XeLaTeX")))
 
   :config
   (use-package tex-buf
     :init
-    ;; (setq TeX-show-compilation t)
+    (setq TeX-show-compilation nil)
     (setq TeX-save-query nil))
 
   (use-package auctex-latexmk
     ;; tlmgr install latexmk
-    :init
+    :config
     (auctex-latexmk-setup))
   
   (use-package cdlatex
-    :hook (LaTeX-mode . turn-on-cdlatex)))
+    :hook (LaTeX-mode . turn-on-cdlatex))
+  
+  (use-package company-auctex
+    :config
+    (company-auctex-init)))
 
 (use-package latex-extra
   :hook (LaTeX-mode . latex-extra-mode))
-
-(use-package company-auctex
-  :init
-  (company-auctex-init))
-
-(use-package company-math
-  :hook (LaTeX-mode . (lambda ()
-                        (add-to-list 'company-backends 'company-math-symbols-unicode))))
 
 (provide 'init-tex)
 
