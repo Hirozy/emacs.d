86ac20f5d672cf00c52f4d7583873b5a6b294695
Hirozy <yoohwzy@gmail.com>
2020-10-07 01:18:24 +0800

    Add package: awesome-tab; Use doom-theme for treemacs.

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -30,7 +30,8 @@
   :init
   ;; Set the time delay (in seconds) for the which-key popup to appear. A value of
   ;; zero might cause issues so a non-zero value is recommended.
-  (setq which-key-idle-delay 0.1)
+  (setq which-key-idle-delay 0.1
+        which-key-popup-type 'minibuffer)
   :config
   (which-key-mode 1))
 
@@ -78,6 +79,51 @@
 
 (use-package open-newline
   :load-path "site-lisp/open-newline")
+
+(use-package awesome-tab
+  :load-path "site-lisp/awesome-tab"
+  :config
+  (setq awesome-tab-display-icon nil
+        awesome-tab-height 130)
+
+  (defun awesome-tab-hide-tab (x)
+    "Hide tabs"
+    (let ((name (format "%s" x)))
+      (or
+       (string-prefix-p "*ccls" name)
+       (string-prefix-p "*Compile-Log*" name)
+       (string-prefix-p "*lsp" name)
+       (string-prefix-p "*pyright" name)
+       (string-prefix-p "*which" name)
+       (and (string-prefix-p "magit" name)
+            (not (file-name-extension name))))))
+  (setq awesome-tab-hide-tab-function 'awesome-tab-hide-tab)
+
+  (defun awesome-tab-buffer-groups ()
+    "`awesome-tab-buffer-groups' control buffers' group rules.
+Group awesome-tab with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
+All buffer name start with * will group to \"Emacs\".
+Other buffer group by `awesome-tab-get-group-name' with project name."
+    (list
+     (cond
+      ((or (string-equal "*" (substring (buffer-name) 0 1))
+           (memq major-mode '(magit-process-mode
+                              magit-status-mode
+                              magit-diff-mode
+                              magit-log-mode
+                              magit-file-mode
+                              magit-blob-mode
+                              magit-blame-mode)))
+       "Emacs")
+      ((derived-mode-p 'eshell-mode)
+       "EShell")
+      ((derived-mode-p 'dired-mode)
+       "Dired")
+      (t
+       (awesome-tab-get-group-name (current-buffer))))))
+  (setq awesome-tab-buffer-groups-function 'awesome-tab-buffer-groups)
+
+  (awesome-tab-mode t))
 
 (provide 'init-utils)
 
