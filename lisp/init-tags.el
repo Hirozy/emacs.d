729198172a31e3ad83d6417ebbb6821698507a72
Hirozy <me@hirozy.com>
2020-10-30 01:56:04 +0800

    Add some key binds.

--- lisp/init-tags.el
+++ lisp/init-tags.el
@@ -14,11 +14,16 @@
                     counsel-gtags))
 
 (use-package ggtags
-  :bind (("C-c C-g s" . ggtags-find-other-symbol)
-         ("C-c C-g h" . ggtags-view-tag-history)
-         ("C-c C-g f" . ggtags-find-reference)
-         ("C-c C-g c" . ggtags-create-tags)
-         ("C-c C-g u" . ggtags-update-tags))
+  :bind (("C-c C-g C-r" . ggtags-find-reference)
+         ("C-c C-g C-d" . ggtags-find-definition)
+         ("C-c C-g C-c" . ggtags-find-tag-dwim)
+         ("C-c C-g C-e" . ggtags-find-tag-regexp)
+         ("C-c C-g C-s" . ggtags-find-other-symbol)
+         ("C-c C-g C-f" . ggtags-find-file)
+         ("C-c C-g C-p" . ggtags-query-replace)
+         ("C-c C-g C-h" . ggtags-view-tag-history)
+         ("C-c C-g C-c" . ggtags-create-tags)
+         ("C-c C-g C-u" . ggtags-update-tags))
   :hook ((c-mode
           c++-mode
           python-mode) . ggtags-mode))
