542b86e3b826c3e3a9faa9cfbcfb34adeb1330b0
Hirozy <me@hirozy.com>
2021-05-14 00:39:45 +0800

    Add javascript mode

---
+++ lisp/init-javascript.el
@@ -0,0 +1,18 @@
+;;; init-javascript.el --- javascript-mode configuratione
+
+;;; Commentary:
+;;
+;; javascript-mode configuration
+;;
+
+;;; Require
+(require 'init-packages)
+
+;;; Code:
+
+(require-packages '(js2-mode))
+
+
+(provide 'init-javascript)
+
+;;; init-javascript.el ends here
