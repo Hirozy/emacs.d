f563e68c4b9b55a3d344e40bbdc794b32de660e9
Hirozy <me@hirozy.com>
2022-04-17 23:21:03 +0800

    Add package: citre

--- lisp/init-tags.el
+++ lisp/init-tags.el
@@ -10,7 +10,27 @@
 
 ;;; Code:
 
-(require-packages '(xref))
+(require-packages '(citre
+                    xref))
+
+(use-package citre
+  :after projectile
+  :defer t
+  :bind (("C-x c j" . citre-jump)
+         ("C-x c J" . citre-jump-back)
+         ("C-x c p" . citre-peek)
+         ("C-x c P" . citre-ace-peek)
+         ("C-x c e" . citre-edit-tags-file-recipe)
+         ("C-x c u" . citre-update-this-tags-file))
+  :init
+  (require 'citre-config)
+
+  :config
+  (setq citre-project-root-function #'projectile-project-root
+        citre-default-create-tags-file-location 'global-cache
+        citre-use-project-root-when-creating-tags t
+        citre-prompt-language-for-ctags-command t
+        citre-auto-enable-citre-mode-modes '(prog-mode)))
 
 (use-package xref
   :bind (("<f12>" . xref-find-definitions)
