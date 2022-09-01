98e3bfdfebb97a72c400978b6f17d40c484fbf29
Hirozy <me@hirozy.com>
2022-09-02 00:17:47 +0800

    Add function "set-current-path-to-pythonpath"

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -7,6 +7,18 @@
 
 ;;; Code:
 
+(defun set-current-path-to-pythonpath (prompt)
+  "Add current path to PYTHONPATH, if PROMPT is non-nil, reset current path to PYTHONPATH."
+  (interactive "P")
+  (let ((current-path (expand-file-name default-directory)))
+    (if current-prefix-arg
+        (progn
+          (message "Reset %S to PYTHONPATH" current-path)
+          (setenv "PYTHONPATH" current-path))
+      (progn
+        (message "Add %S to PYTHONPATH" current-path)
+        (setenv "PYTHONPATH" (concat current-path ":" (getenv "PYTHONPATH")))))))
+
 (setq python-shell-completion-native-enable nil)
 
 (use-package conda
