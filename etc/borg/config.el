d40bd3a2703dcccba27aa9acdcf80946317e3345
Hirozy <me@hirozy.com>
2023-03-16 22:17:43 +0800

    Do not compile `vterm` on Windows

--- etc/borg/config.el
+++ etc/borg/config.el
@@ -1,7 +1,7 @@
 (when (native-comp-available-p)
   (setq package-native-compile t))
 
-(when (require 'vterm nil t)
+(when (and (memq window-system '(mac ns x)) (require 'vterm nil t))
   (setq vterm-always-compile-module t))
 
 (when (require 'evil-collection nil t)
