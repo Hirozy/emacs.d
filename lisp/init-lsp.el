0bc2e0a44d12f09bb391f8044ec82c7e7b4aab50
Hirozy <git@hirozy.com>
2026-03-02 05:24:43 +0800

    refactor(completion): adjust CAPF definitions
    Removed the `capf-based-list` variable and directly defined the
    completion-at-point-functions (CAPF) lists in `init-completion.el`
    and `init-lsp.el`. This simplifies the CAPF configuration by
    avoiding an extra layer of indirection.
    
    Additionally, adjusted the `cape-dabbrev-min-length` to 3 and
    changed the `tempel-trigger-prefix` to `<` from `;`.

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -7,15 +7,24 @@
 
 ;;; Code:
 
+(require 'cape)
+
 (defun cape-capf-at-eglot ()
+  "Configure completion-at-point functions based on major mode.
+For C/C++/Objective-C modes, combine eglot and citre completions.
+For other modes, use eglot completion only.
+Includes template expansion and file path completion in all cases.
+The function is described at
+https://github.com/minad/corfu/wiki#making-a-cape-super-capf-for-eglot"
   (if (member major-mode '(c-mode c++-mode objc-mode))
       (setq-local completion-at-point-functions
-                  `(,@capf-based-list
-                    ,(cape-super-capf
-                      #'eglot-completion-at-point #'citre-completion-at-point)))
+                  (list #'tempel-expand
+                        #'cape-file
+                        (cape-capf-super #'eglot-completion-at-point #'citre-completion-at-point)))
     (setq-local completion-at-point-functions
-                `(,@capf-based-list
-                  eglot-completion-at-point))))
+                (list #'tempel-expand
+                      #'cape-file
+                      #'eglot-completion-at-point))))
 
 (use-package eglot
   :commands (eglot-ensure eglot)
