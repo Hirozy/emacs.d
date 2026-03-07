1b7e2c74f1f37a92f6609b1db9ac8b0b9e4b0bdf
Hirozy <git@hirozy.com>
2026-03-07 20:51:31 +0800

    fix(emacs): Address gccemacs initialization issues on macOS
    This commit introduces a workaround for gccemacs on macOS when
    installed via MacPorts.
    
    - Sets `LIBRARY_PATH` to ensure proper execution of gccemacs.
    - Disables asynchronous compilation with
    - `native-comp-jit-compilation nil`.  Blacklists
    - `tramp-loaddefs.el.gz` from native compilation to prevent
    - recompilation at each startup.
    -  The variables `inhibit-automatic-native-compilation` and
    - `inhibit-native-compilation` are no longer exist in the latest
    - Emacs(30+), use `native-comp-jit-compilation nil` instead.

--- early-init.el
+++ early-init.el
@@ -8,6 +8,16 @@
 ;;; Code:
 (setq load-prefer-newer t)
 
+;; WORKAROUND: Add library paths to LIBRARY_PATH to ensure gccemacs can execute properly.
+;; NOTE: These paths should be updated whenever gcc in MacPorts is upgraded.
+(when (and (eq system-type 'darwin)
+           (native-comp-available-p))
+  (setenv "LIBRARY_PATH" (string-join
+                          '("/opt/ports/lib/gcc15"
+                            "/opt/ports/lib/libgcc"
+                            "/opt/ports/lib/gcc15/gcc/aarch64-apple-darwin25/15.2.0")
+                          ":")))
+
 (let ((dotfiles-dir (file-name-directory (or load-file-name buffer-file-name))))
   (add-to-list 'load-path (expand-file-name "lib/packed" dotfiles-dir))
   (add-to-list 'load-path (expand-file-name "lib/compat" dotfiles-dir))
@@ -21,12 +31,21 @@
 (setq package-enable-at-startup nil)
 
 (with-eval-after-load 'package
-                      (setq package-archives
-                            '(("gnu" . "https://elpa.gnu.org/packages/")
-                              ("nongnu" . "https://elpa.nongnu.org/nongnu/"))))
+  (setq package-archives
+        '(("gnu" . "https://elpa.gnu.org/packages/")
+          ("nongnu" . "https://elpa.nongnu.org/nongnu/"))))
 
 (when (native-comp-available-p)
-  (setq inhibit-automatic-native-compilation t))
+  ;; Do not compile .elc files asynchronously.
+  ;; `inhibit-automatic-native-compilation' and `inhibit-native-compilation'
+  ;; appear to be reverted from the Emacs source code
+  (setq native-comp-jit-compilation nil)
+
+  ;; WORKAROUND: On MacPorts with nativecomp enabled, tramp-loaddefs.el.gz is recompiled
+  ;; at every startup. This package is blacklisted to avoid the overhead.
+  (setq native-comp-jit-compilation-deny-list
+        (append native-comp-jit-compilation-deny-list
+                '("tramp-loaddefs.el.gz"))))
 
 ;; Local Variables:
 ;; no-byte-compile: t
