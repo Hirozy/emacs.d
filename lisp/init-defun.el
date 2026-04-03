ab1b264e9960b8cf6c0383296b0ef487e208611c
Hirozy <git@hirozy.com>
2026-04-04 00:10:43 +0800

    refactor(init-defun): Simplify message formatting in helper functions
    - Rename functions with 'guess' prefix for consistency Consolidate
    - repeated message calls into single statements Improve
    - toggle-cape-dict logic with clearer enabled state tracking

--- lisp/init-defun.el
+++ lisp/init-defun.el
@@ -7,20 +7,18 @@
 
 ;;; Code:
 
-(defun defined/derived-mode-prog-or-text-mode ()
+(defun defined/guess-derived-mode-prog-or-text-mode ()
   "Detect if the current major mode is derived from `prog-mode' or `text-mode'."
   (interactive)
-  (cond ((derived-mode-p 'prog-mode)
-         (message "Major mode is prog-mode."))
-        ((derived-mode-p 'text-mode)
-         (message "Major mode is text-mode."))
-        (t
-         (message "Major mode is neither prog-mode or text-mode."))))
+  (message "Major mode is %s."
+           (cond ((derived-mode-p 'prog-mode) "prog-mode")
+                 ((derived-mode-p 'text-mode) "text-mode")
+                 (t "neither prog-mode or text-mode"))))
 
-(defun defined/get-buffer-file-path ()
+(defun defined/guess-buffer-file-path ()
   "Echo current buffer file full path."
   (interactive)
-  (message buffer-file-name))
+  (message "%s" buffer-file-name))
 
 (defalias 'defined/cape-dict-min (cape-capf-prefix-length #'cape-dict 4))
 
@@ -30,12 +28,12 @@
 If `cape-dict' is currently active in `completion-at-point-functions'
 for this buffer, it will be removed. Otherwise, it will be added."
   (interactive)
-  (if (memq #'defined/cape-dict-min completion-at-point-functions)
-      (progn
-        (remove-hook 'completion-at-point-functions #'defined/cape-dict-min t)
-        (message "Disable `cape-dict' completion in this buffer"))
-    (add-hook 'completion-at-point-functions #'defined/cape-dict-min nil t)
-    (message "Enable `cape-dict' completion in this buffer")))
+  (let ((enabled (not (memq #'defined/cape-dict-min completion-at-point-functions))))
+    (if enabled
+        (add-hook 'completion-at-point-functions #'defined/cape-dict-min nil t)
+      (remove-hook 'completion-at-point-functions #'defined/cape-dict-min t))
+    (message "%s `cape-dict' completion in this buffer"
+             (if enabled "Enable" "Disable"))))
 
 (defun defined/guess-which-org-publish-project ()
   "Display and return the org-publish project name for the current Org file.
